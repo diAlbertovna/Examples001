bc862add907436420b3754e87821c450931f7a37
Diana <dianadx24@gmail.com>
2023-07-03 22:38:01 +0300

    005

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "diana")
+{
+    Console.WriteLine("My Dear Diana!");
+}
+else
+{
+    Console.Write("Hi");
+    Console.WriteLine(username);
+}
